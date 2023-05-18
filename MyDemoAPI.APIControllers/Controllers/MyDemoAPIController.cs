b1b89233ab15641e3e7b7dcb08ebab867bdcee9f
Thushara Rathnaweera <thushara@virtusales.com>
2023-05-18 10:08:22 +0100

    Add project files.

---
+++ MyDemoAPI.APIControllers/Controllers/MyDemoAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyDemoAPI.APIControllers.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyDemoAPIController : ControllerBase
+    {
+        private readonly ILogger<MyDemoAPIController> _logger;
+        public MyDemoAPIController(ILogger<MyDemoAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetDemoAPIResult()
+        {
+            try
+            {
+                return Ok("Hello from MyDemoAPI");
+            }
+            catch
+            {
+                _logger.LogError("Error ocurred in GetDemoAPIResult()");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
